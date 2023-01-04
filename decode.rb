921a0598a742cb757531614017fd786fd1ac727d
asnganje <abd.nganje@gmail.com>
2023-01-04 08:15:00 +0300

    decode morse

--- decode.rb
+++ decode.rb
@@ -5,27 +5,29 @@ MORSE = {
   '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
   '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
   '--..' => 'Z'
-}.freeze
+}
 
 def decode_char(char)
-  @theLetter = MORSE[char].downcase
-  @theLetter
+  decoded = MORSE[char]
+  if decoded
+    decoded.downcase
+  else
+    "Not a morse code"
+  end
 end
 
 def decode_word(word)
-    @letters = word.split
-    @ourLetters = ''
-  @letters.each do |char|
-    decode_char(char)
-    @ourLetters += decode_char(char)
-  end
-  puts @ourLetters
+  n_word = word.split(" ")
+  n_word.map do |char| decode_char(char) end.join
 end
 
 def decode_sentence(sentence)
-  sentence.split.each do |word|
-    decode_word(word)
-  end
+  @sentns = sentence.split("   ")
+  @sentns.map do |word| 
+    decode_word(word) 
+  end.join(" ")
 end
-decode_char('--..')
-decode_word('--.. ..')
+
+puts decode_char(".-")
+puts decode_word("-- -.--")
+puts decode_sentence("-- -.--   -. .- -- .")
